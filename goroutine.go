0cf943a882925a98c4bfd7bcf0c32269d6b545b5
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2023-01-07 16:13:36 +0330

    orders in goroutin

---
+++ goroutine.go
@@ -0,0 +1,27 @@
+package main
+
+/*
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("start")
+	go message("Hi User!")
+	fmt.Println("End")
+	time.Sleep(time.Second)
+}
+
+// each prints has a unique goroutine
+func message(text string) {
+	for i := 1; i <= 10; i++ {
+		go printMessage(text, i)
+	}
+}
+
+//  printe 10 case without order ... Hi User! 1  , Hi User! 5   ,  Hi User! 10 ...
+// no order be considered becuase task devided to multi goroutine
+
+*/
